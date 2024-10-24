4fffc4a0aa4184cee006fa3741226dba942e3db6
dopzhik <haltura24@gmail.com>
2024-10-24 21:20:53 +0800

    HW3

---
+++ model/FamilyElement.java
@@ -0,0 +1,8 @@
+package model;
+
+public interface FamilyElement {
+    String getName();
+    int getAge();
+    String getSuns();
+    String getDaughters();
+}
---
+++ model/iterators/FamilyTreeIterator.java
@@ -0,0 +1,5 @@
+package model.iterators;
+
+public class FamilyTreeIterator {
+
+}
